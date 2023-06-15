1515d4072b71441fff4ee88694a430c174063b5e
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-15 16:51:09 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 1
+ */
+int main(void)
+{
+	fprintf(stdout, "and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+	return (1);
+}
