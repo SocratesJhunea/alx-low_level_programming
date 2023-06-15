89543d5b2e8b2744844e65f8c355117e797d842a
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-15 14:56:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-int main() { puts("Programming is like building a multilingual puzzle"); return 0; }
+int main(void) { return puts("Programming is like building a multilingual puzzle"), 0; }
