acc1ca1aaf83a9ea53ede57c58e3979ac7513af1
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-15 14:59:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the message "Programming is like building a multilingual puzzle"
+ *              using the puts function.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void) { return puts("Programming is like building a multilingual puzzle"), 0; }
