9572c35356662403bde304f6874dea7accfd2416
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-15 19:46:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fput(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
