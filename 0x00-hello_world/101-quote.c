dd63bd0e8405dcc30fd3a43e6b5ce3bf7e87cc30
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-15 17:20:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    fwrite(message, sizeof(char), sizeof(message) - 1, stderr);
-    return 1;
+fwrite(message, sizeof(char), sizeof(message) - 1, stderr);
+return (1);
 }
