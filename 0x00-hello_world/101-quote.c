2c3be8266ef75a6cd406d55def6a774a815c7480
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-15 19:05:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ * Return: Always 1 (failure)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar 2015-10-19\n", stdout);
+	const char *msg = "and that piece of art is useful - Dora Korpar, 2015-10-19";
+fwrite(msg, sizeof(char), sizeof(msg) - 1, stdout);
 return (1);
 }
+
