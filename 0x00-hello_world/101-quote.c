1bc5ef64f70419891784a0f95103cfb2786ab4db
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-15 19:25:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Return: Always 1 (failure)
+ * Return: Always 1 (error)
  */
 int main(void)
 {
-	const char *m = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-fwrite(m, sizeof(char), sizeof(m) - 1, stdout);
-return (1);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, "
+			"2015-10-19\n");
+	return (1);
 }
-
