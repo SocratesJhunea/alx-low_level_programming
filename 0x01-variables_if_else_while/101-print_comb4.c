2bce9d014339dab245bf0450619e9a8e2bf9fc05
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-16 18:56:32 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,15 +9,17 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = i + 1; j < 3; j++)
+		for (j = i + 1; j < 9; j++)
 		{
-			for (k = j + 1; k < 3; k++)
+			for (k = j + 1; k < 10; k++)
 			{
-				putchar(i * 100 + j * 10 + k + '0');
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 
-				if (i != 0 || j != 1 || k != 2)
+				if (i != 7 || j != 8 || k != 9)
 				{
 					putchar(',');
 					putchar(' ');
