e360a7d8e332142a14f094681ddc485bc1158bba
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-16 17:26:33 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - function to print combination of two numbers
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int digA = '0';
+	int digB = '0';
+
+	while (digA <= 9)
+	{
+		while (digB <= '9')
+		{
+			if (!(digA > digB) || digA == digB)
+			{
+				putchar(digA);
+				putchar(digB);
+				if (digA == '8' && digB == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+digB++;
+		}
+	}
+digB = '0';
+digA++;
+return (0);
+}
