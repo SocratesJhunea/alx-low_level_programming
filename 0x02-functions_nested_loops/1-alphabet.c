0d93a8f8eadcda6145681dc92b06166115ce2981
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-20 05:14:08 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * printAlphabet - A function that prints the alphabet, in lowercase.
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	_putchar(letter);
+	letter++;
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: void.
+ */
+
+int main(void)
+{
+	print_alphabet();
+return (0);
+}
