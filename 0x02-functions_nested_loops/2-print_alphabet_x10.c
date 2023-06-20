b2007343264d56194105124d140f5eb497a5d8e1
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-20 06:01:35 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,18 @@
 
 void print_alphabet_x10(void)
 {
-	char letter = 'a';
-	int count = 0;
 
-	while (count < 10)
-	{
-	for (letter = 'a'; letter <= 'z'; letter++)
-		{
-		_putchar(letter);
-		}
-	count++;
-	}
+char letter = 'a';
+int count = 0;
 
-	_putchar('\n');
+while (count < 10)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+count++;
+}
+
 }
