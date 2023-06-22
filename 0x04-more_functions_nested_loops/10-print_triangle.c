4afa60c5de26c2dae0f0e561ca6e2fc3234684d5
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-22 09:08:13 -0700

    10-print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,22 +4,26 @@
  * print_triangle - function that prints a triangle
  * @size:  Is the size of the triangle
  *
- * Return: 0 Always
+ * Return: void
  */
 
 void print_triangle(int size)
 {
+	int row, hashes, spaces;
+
 	if (size <= 0)
 	{
 	_putchar('\n');
 	}
 	else
 	{
-	int i, j;
-
-	for (i = 0; i < size; i++)
+	for (row = 1; row <= size; row++)
 	{
-	for (j = 0; j <= i; j++)
+	for (spaces = size - row; spaces >= 1; spaces--)
+	{
+	_putchar(' ');
+	}
+	for (hashes = 1; hashes <= row; hashes++)
 	{
 	_putchar('#');
 	}
