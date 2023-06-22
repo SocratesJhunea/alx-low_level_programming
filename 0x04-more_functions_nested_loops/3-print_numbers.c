8c99ab2693e7d371dd8186738195ef54cfdd7cae
SocratesJhunea <kirkfrankestein@gmail.com>
2023-06-22 06:14:35 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers since 0 to 9
+ * print_numbers - Prints the numbers since 0 up to 9
  *
- * Return: The numbers since 0 to 9
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
+
 {
 
 	int x;
