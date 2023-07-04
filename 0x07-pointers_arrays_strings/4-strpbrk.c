02cce48ef5cba419bc0f36cb19ce9ea45f89dde5
SocratesJhunea <kirkfrankestein@gmail.com>
2023-07-04 04:14:50 -0700

    4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+
+{
+
+	int j;
+
+	while (*s)
+
+	{
+
+		for (j = 0; accept[j]; j++)
+
+		{
+
+		if (*s == accept[j])
+
+		return (s);
+
+		}
+
+	s++;
+
+	}
+
+	return ('\0');
+
+}
