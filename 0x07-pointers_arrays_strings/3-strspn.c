d7fedad491b3773c616e8a99d573d4a6479ff92d
SocratesJhunea <kirkfrankestein@gmail.com>
2023-07-04 04:08:41 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,35 +12,22 @@ unsigned int _strspn(char *s, char *accept)
 {
 
 	unsigned int n = 0;
-
 	int r;
 
 	while (*s)
-
 	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
 
-	for (r = 0; accept[r]; r++)
-
-	}
-
-	if (*s == accept[r])
-
-	{
-
-	n++;
-
-	break;
-
-	}
-
-	else if (accept[r + 1] == '\0')
-
-	return (n);
-
-	}
-
-	s++;
-
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
 	}
 
 	return (n);
