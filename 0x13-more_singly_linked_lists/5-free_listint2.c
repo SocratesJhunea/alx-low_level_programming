869f91534286213749e5e7fe2ce04e6d3d843fd7
SocratesJhunea <kirkfrankestein@gmai.com>
2023-08-01 10:24:24 -0700

    5-free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current;
+
 	if (head == NULL)
 	return;
 
 	while (*head)
 	{
-		current =(*head)->next;
-		free = (head);
-		current = *head;
+		current = (*head)->next;
+
+		free(head);
+		*head = current;
 	}
 
 	*head = NULL;
